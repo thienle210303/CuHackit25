a5703f745f9cc10c39e13e412813cb9a6bb9f66c
thienle210303 <thienle210303@gmail.com>
2025-03-07 22:06:58 -0500

    init map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Map from "./components/Map"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Map>
+
+    </Map>
   )
 }
 
