abe08975f7af3d083c245c9d8df1d3606e361560
My Tran <105460300+mmtran0210@users.noreply.github.com>
2025-03-08 16:16:20 -0500

    Create AppDbContext.cs

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>()
+                .HasKey(e => e.Id); // Primary key for Event table
+        }
+    }
+}
